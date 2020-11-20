d2c0b6b6a946f96ef859b20e25ae07f1790aeb1e
Murat Ozer <m.ozer@uc.edu>
2020-11-20 09:22:26 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { constants } = require('crypto');
 
 
 var options = {
-    key: fs.readFileSync('infoSEC.key'),
+    key: fs.readFileSync('infoSEC.key', 'Murat2008'),
     cert: fs.readFileSync('infoSEC.crt'),
     ca: fs.readFileSync('infoSEC.ca-bundle'),
     secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_TLSv1,
